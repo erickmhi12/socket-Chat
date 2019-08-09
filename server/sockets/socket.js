aef0b74e5ebd89eb24e2a2342e0602f8474b1a80
Erick <ErickH@translead.com>
2019-08-08 18:29:17 -0700

    First commit:

---
+++ server/sockets/socket.js
@@ -0,0 +1,55 @@
+const { io } = require('../server');
+
+const { Usuario } = require('../classes/usuarios');
+
+const { crearMensaje } = require('../utilidades/utilidades');
+
+const usuarios = new Usuario();
+
+
+io.on('connection', (client) => {
+
+    client.on('EntrarChat', (data, callback) => {
+
+
+            if (!data.nombre || !data.sala) {
+                return callback({
+                    error: true,
+                    mensaje: 'El nombre/sala es necesario'
+                });
+            }
+
+            client.join(data.sala);
+
+            usuarios.agregarPersona(client.id, data.nombre, data.sala);
+
+            client.broadcast.to(data.sala).emit('listaPersona', usuarios.getPersonasPorSala(data.sala));
+
+            callback(usuarios.getPersonasPorSala(data.sala));
+
+        })
+        //Se utiliza el metodo para enviar mensajes a todos los usuarios
+    client.on('crearMensaje', (data) => {
+        let persona = usuarios.getPersona(client.id);
+
+        let mensaje = crearMensaje(persona.nombre, data.mensaje);
+        client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+    })
+
+    // Se activa cuando el usuario se desconecta
+    client.on('disconnect', () => {
+        let personaBorrada = usuarios.borrarPersona(client.id);
+
+        client.broadcast.to(personaBorrada.sala).emit('crearMensaje', crearMensaje('Administrador', `${ personaBorrada.nombre } salio`));
+        client.broadcast.to(personaBorrada.sala).emit('listaPersona', usuarios.getPersonasPorSala(personaBorrada.sala));
+
+    });
+
+    //Mensajes Privados
+    client.on('mensajePrivado', data => {
+
+        let persona = usuarios.getPersona(client.id);
+
+        client.broadcast.to(data.para).emit('mensajePrivado', crearMensaje(persona.nombre, data.mensaje));
+    })
+});
